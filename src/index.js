4ca9797b1a3a1bbd887eb622f75d7b89a63c57f3
Lrapava <luka.rapava2004@gmail.com>
2022-11-21 02:52:30 +0400

    optimizations

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
  * React Bootstrap Configuration
 */
 
-import "../node_modules/react-bootstrap/dist/react-bootstrap.js"
+import "../node_modules/react-bootstrap/dist/react-bootstrap.min.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
